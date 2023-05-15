afeeceda139bac8c1740b59d907daa5198e4f715
giuseppe bruda <giuseppebruda10@gmail.com>
2023-05-15 13:07:59 -0300

    segunda pre-entrega lista

--- js/main.js
+++ js/main.js
@@ -26,3 +26,46 @@ function addUser () {
 addUser();
 
 alert(`el usuario se creo con exito bienvenido`)
+
+/* lista de productos a la venta */
+const products = [
+    {id:1, marca:"nike", modelo: "nike cortez" , precio: 12000},
+    {id:2, marca:"adidas", modelo: "shark" , precio:15000 },
+    {id:3, marca:"jordan", modelo:"jordan retro 5" , precio:20000 },
+    {id:4, marca:"adidas", modelo: "yezzy" , precio:25000 },
+]
+
+alert("genial para continuar te vamos a mostrar una lista de nuestros productos")
+
+
+/* usando forEach para recorrer el objeto y mostrarlo por alert */
+products.forEach(item=>{
+    alert(`
+    id: ${item.id}
+    marca: ${item.marca}
+    modelo: ${item.modelo}
+    precio: $${item.precio}
+    `)
+})
+
+let selection = parseInt(prompt("ingresa el ID del producto que quieres comprar"))
+const sum = (a,b) => a + b;
+const subtraction = (a,b) => a - b;
+const iva = x => x * 0.21;
+const discount = 1500;
+
+for(const product of products){
+    if(product.id === selection){
+        alert(`excelente eleccion las ${product.modelo} tambien son mis favoritas`)
+        const reduction = subtraction(sum(product.precio, iva(product.precio)),discount)
+        alert(`el precio de tus ${product.modelo} luego del descuento del hot sale es de ${reduction}`)
+        alert ("gracias por visitarnos")
+    }
+}
+
+
+
+
+
+
+
